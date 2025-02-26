2698babe5d86dff9f5a2b3601eeee41da96a4c75
Riku458 <jedrickpogi69@gmail.com>
2025-02-26 21:43:36 +0800

    try 5.2

--- 1st_project.py
+++ 1st_project.py
@@ -3,7 +3,7 @@
 import os # this is for directory of the file downloaded
 import threading # this is for running download process without complications
 import tkinter as tk # this is for the GUI library
-from tkinter import messagebox # this will be important for notification of what happened in the file and downloading process
+from tkinter import messagebox, filedialog # this will be important for notification of what happened in the file and downloading process
 import yt_dlp # this is for downloading the youtube links (I changed it into yt_dlp)
 
 # 2. Create a funtion that will get the youtube link
@@ -16,10 +16,39 @@ def dl_youtube():
             return
         format_choice = format_var.get()
         quality_choice = quality_var.get()
-        download_folder = "Downloads"
-        os.makedirs(download_folder, exist_ok=True)
-
         
+        download_folder = filedialog.askdirectory(title="Select Download Folder")
+        if not download_folder:
+            messagebox.showerror("Error", "Please select or create a download folder")
+            return
+
+        os.makedirs(download_folder, exist_ok=True)
+        
+
+        video_quality_map = {
+            "144p": "bv*[height=144]+ba/b[height=144]",
+            "240p": "bv*[height=240]+ba/b[height=240]",
+            "360p": "bv*[height=360]+ba/b[height=360]",
+            "480p": "bv*[height=480]+ba/b[height=480]",
+            "720p": "bv*[height=720]+ba/b[height=720]",
+            "1080p": "bv*[height=1080]+ba/b[height=1080]",
+            "Best": "bestvideo+bestaudio/best"
+        }
+
+        audio_quality_map = {
+            "64kbps": "64",
+            "128kbps": "128",
+            "192kbps": "192",
+            "256kbps": "256",
+            "320kbps": "320",
+            "Best": "best"
+        }
+
+        if format_choice == "mp4"
+            selected_format = video_quality_map.get(quality_choice, "bestvideo+bestaudio/best")
+            yt_opts = 
+
+    threading.Thread(target=task).start()
 
 # 3. Create a function that will find the quality and format of file of what the user's want to download
 
