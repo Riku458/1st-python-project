5b602711bd646a667c15ad4f7546c14ce2f79024
Riku458 <jedrickpogi69@gmail.com>
2025-02-26 16:40:25 +0800

    try 3

--- 1st_project.py
+++ 1st_project.py
@@ -1,4 +1,9 @@
 # 1. Import important modules
+import os
+import threading
+import tkinter as tk
+from tkinter import messagebox
+import pytube 
 # 2. Create a funtion that will get the youtube link
 # 3. Create a function that will find the quality and format of file of what the user's want to download
 # 4. Create a funtion that will implement that quality of what the user want
