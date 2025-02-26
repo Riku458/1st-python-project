bf6a6d914283eff4b4bd4835e7ebbd1168bc50b0
Riku458 <jedrickpogi69@gmail.com>
2025-02-26 16:37:33 +0800

    try 2

---
+++ 1st_project.py
@@ -0,0 +1,5 @@
+# 1. Import important modules
+# 2. Create a funtion that will get the youtube link
+# 3. Create a function that will find the quality and format of file of what the user's want to download
+# 4. Create a funtion that will implement that quality of what the user want
+# 5. Create a GUI of the project (Height, Width, Button, and Input Box)
